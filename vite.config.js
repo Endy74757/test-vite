1bd51287e96a4a4acc1b1f90c3a6537db94ac0aa
Endy <jlbzaza2@gmail.com>
2024-07-31 15:31:08 +0700

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+
 export default {
-    root: '.',
-    base: './',
-    build: {
-      outDir: 'dist',
-    },
-  };
+  // กำหนด directory ที่ใช้เป็น root ของโปรเจกต์
+  root: '.',
+
+  // กำหนด base path สำหรับการสร้างไฟล์ build
+  base: './',
+
+  // กำหนดการตั้งค่าสำหรับการสร้างไฟล์ build
+  build: {
+    outDir: 'dist', // กำหนด directory สำหรับไฟล์ build ที่สร้างขึ้น
+  },
+
+  // การตั้งค่าเซิร์ฟเวอร์
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+  },
+};
